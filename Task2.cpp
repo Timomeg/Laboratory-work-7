119dad7680dd6a409cc9dc1a7965a240790dbc4a
Timofey Rotin <162833896+Timomeg@users.noreply.github.com>
2024-11-23 18:50:49 +0300

    Task2

--- Task2.cpp
+++ Task2.cpp
@@ -3,8 +3,12 @@
 void saveToFile(const std::string& filename, const std::vector<Book>& data) {
     std::ofstream out;
     out.open(filename, std::ios::app);
-    for (const auto& str: data) {
-        //out << str << std::endl;
+    if (out.is_open()) {
+        for (const auto& book: data) {
+            out << book.Author << "\n";
+            out << book.Title << "\n";
+            out << book.Year << std::endl;
+        }
     }
     out.close();
 }
@@ -12,10 +16,29 @@ void saveToFile(const std::string& filename, const std::vector<Book>& data) {
 void loadFromFile(const std::string& filename, std::vector<Book>& outData) {
     std::ifstream in;
     std::string line;
+    int i{};
+    std::string author;
+    std::string title;
+    int year{0};
 
     in.open(filename, std::ios::in);
-    while(std::getline(in, line)) {
-        //outData.push_back(line);
+    if (in.is_open()) {
+        while(std::getline(in, line)) {
+            if (i % 3 == 0) {
+                author = line;
+            }
+            if (i % 3 == 1) {
+                title = line;
+            }
+            if (i % 3 == 2) {
+                for (auto j: line) {
+                    year = year * 10 + static_cast<int>(j);
+                }
+                Book book_i{author, title, year};
+                outData.push_back(book_i);
+            }
+            i++;
+        }
     }
     in.close();
 
