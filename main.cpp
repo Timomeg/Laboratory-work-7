b3805869195780cfa6368f007e51410a594578a9
Timofey Rotin <162833896+Timomeg@users.noreply.github.com>
2024-11-25 02:11:07 +0300

    clear main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,4 @@
 #include "header.h"
 int main() {
-    std::ofstream out;
-    out.open("lol.txt", std::ios::app);
-    const std::vector<std::string> str= {"daadasd", "dasdas"};
-    saveToFile("lol.txt", str);
-    out.close();
     return 0;
 }
